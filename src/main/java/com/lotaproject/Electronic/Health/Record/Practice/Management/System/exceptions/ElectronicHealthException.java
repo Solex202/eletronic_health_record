d5b514bd174934eb0d0f2eecfb91c157a88a673d
Lotachukwu Onwuka <christiansolomon592@gmail.com>
2023-06-28 10:08:46 +0100

    creating exception package

---
+++ src/main/java/com/lotaproject/Electronic/Health/Record/Practice/Management/System/exceptions/ElectronicHealthException.java
@@ -0,0 +1,8 @@
+package com.lotaproject.Electronic.Health.Record.Practice.Management.System.exceptions;
+
+public class ElectronicHealthException extends RuntimeException{
+
+    public ElectronicHealthException(String message){
+        super(message);
+    }
+}
---
+++ src/main/java/com/lotaproject/Electronic/Health/Record/Practice/Management/System/exceptions/ExceptionMessages.java
@@ -0,0 +1,4 @@
+package com.lotaproject.Electronic.Health.Record.Practice.Management.System.exceptions;
+
+public enum ExceptionMessages {
+}
