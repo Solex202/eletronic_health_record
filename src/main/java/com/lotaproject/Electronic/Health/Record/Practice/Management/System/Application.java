9ab1882d9dc726c5452f4bbb791c298365ce9edf
Lotachukwu Onwuka <christiansolomon592@gmail.com>
2023-06-28 09:13:28 +0100

    application set up

---
+++ src/main/java/com/lotaproject/Electronic/Health/Record/Practice/Management/System/Application.java
@@ -0,0 +1,13 @@
+package com.lotaproject.Electronic.Health.Record.Practice.Management.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
