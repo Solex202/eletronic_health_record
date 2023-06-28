3e80544987fcc6988ca6ce591bdfcd6785e06608
Lotachukwu Onwuka <christiansolomon592@gmail.com>
2023-06-28 10:28:12 +0100

    creating packages

---
+++ src/test/java/com/lotaproject/Electronic/Health/Record/Practice/Management/System/service/PatientServiceImplementationTest.java
@@ -0,0 +1,17 @@
+package com.lotaproject.Electronic.Health.Record.Practice.Management.System.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PatientServiceImplementationTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
