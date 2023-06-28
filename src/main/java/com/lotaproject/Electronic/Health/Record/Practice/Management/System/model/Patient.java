4207723c71dc626c5311169d248c731ceacdfac0
Lotachukwu Onwuka <christiansolomon592@gmail.com>
2023-06-28 09:23:30 +0100

    application set up

--- src/main/java/com/lotaproject/Electronic/Health/Record/Practice/Management/System/model/Patient.java
+++ src/main/java/com/lotaproject/Electronic/Health/Record/Practice/Management/System/model/Patient.java
@@ -1,4 +1,27 @@
 package com.lotaproject.Electronic.Health.Record.Practice.Management.System.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Patient {
+
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String address;
+    private String occupation;
+    private LocalDate dob;
+    private Gender gender;
+    private BloodGroup bloodGroup;
+    private Genotype genotype;
 }
