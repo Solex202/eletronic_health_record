4f7b8e58550ac8ceacaf162060cec3c3f61cbb5d
Lotachukwu Onwuka <christiansolomon592@gmail.com>
2023-06-28 10:28:31 +0100

    creating packages

---
+++ src/main/java/com/lotaproject/Electronic/Health/Record/Practice/Management/System/service/PatientServiceImplementation.java
@@ -0,0 +1,11 @@
+package com.lotaproject.Electronic.Health.Record.Practice.Management.System.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class PatientServiceImplementation implements PatientService{
+
+
+}
