2faac239472f953fe3478a7820ab1515772d69ad
Lotachukwu Onwuka <christiansolomon592@gmail.com>
2023-06-28 10:37:53 +0100

    creating packages

--- src/test/java/com/lotaproject/Electronic/Health/Record/Practice/Management/System/service/PatientServiceImplementationTest.java
+++ src/test/java/com/lotaproject/Electronic/Health/Record/Practice/Management/System/service/PatientServiceImplementationTest.java
@@ -2,9 +2,11 @@ package com.lotaproject.Electronic.Health.Record.Practice.Management.System.serv
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class PatientServiceImplementationTest {
 
     @BeforeEach
