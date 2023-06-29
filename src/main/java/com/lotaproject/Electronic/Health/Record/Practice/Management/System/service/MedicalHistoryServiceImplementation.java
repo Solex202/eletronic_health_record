40ca9cfa1daf67f3f62c489397d4410e56ecbbc9
Lotachukwu Onwuka <christiansolomon592@gmail.com>
2023-06-29 21:08:13 +0100

    testing business logic for registering a patient

---
+++ src/main/java/com/lotaproject/Electronic/Health/Record/Practice/Management/System/service/MedicalHistoryServiceImplementation.java
@@ -0,0 +1,24 @@
+package com.lotaproject.Electronic.Health.Record.Practice.Management.System.service;
+
+import com.lotaproject.Electronic.Health.Record.Practice.Management.System.data.model.MedicalHistory;
+import com.lotaproject.Electronic.Health.Record.Practice.Management.System.data.repository.MedicalHistoryRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@AllArgsConstructor
+public class MedicalHistoryServiceImplementation implements MedicalHistoryService {
+
+    @Autowired
+    private MedicalHistoryRepository medicalHistoryRepository;
+
+
+    @Override
+    public void createMedicalHistory(MedicalHistory medicalHistory) {
+        medicalHistoryRepository.save(medicalHistory);
+    }
+}
+
