a426af84d2a961eff06b3eb2329eae3e30883e3d
Lotachukwu Onwuka <christiansolomon592@gmail.com>
2023-06-29 21:10:41 +0100

    testing business logic for registering a patient

---
+++ src/main/java/com/lotaproject/Electronic/Health/Record/Practice/Management/System/exceptions/CannotRegisterPatientException.java
@@ -0,0 +1,7 @@
+package com.lotaproject.Electronic.Health.Record.Practice.Management.System.exceptions;
+
+public class CannotRegisterPatientException extends ElectronicHealthException {
+    public CannotRegisterPatientException(String message) {
+        super(message);
+    }
+}
