05a218db9eb02ccaa95ca657161c4013469413fb
Lotachukwu Onwuka <christiansolomon592@gmail.com>
2023-06-29 21:09:06 +0100

    testing business logic for registering a patient

---
+++ src/main/java/com/lotaproject/Electronic/Health/Record/Practice/Management/System/service/MedicalHistoryService.java
@@ -0,0 +1,8 @@
+package com.lotaproject.Electronic.Health.Record.Practice.Management.System.service;
+
+import com.lotaproject.Electronic.Health.Record.Practice.Management.System.data.model.MedicalHistory;
+
+public interface MedicalHistoryService {
+
+    void createMedicalHistory(MedicalHistory medicalHistory);
+}
