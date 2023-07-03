4b94f2e5172065d27a57525f4cd71d0945f63919
Lotachukwu Onwuka <christiansolomon592@gmail.com>
2023-07-03 21:30:39 +0100

    creating appointment service

---
+++ src/main/java/com/lotaproject/Electronic/Health/Record/Practice/Management/System/service/AppointmentServiceImplementation.java
@@ -0,0 +1,10 @@
+package com.lotaproject.Electronic.Health.Record.Practice.Management.System.service;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AppointmentServiceImplementation implements AppointmentService{
+}
