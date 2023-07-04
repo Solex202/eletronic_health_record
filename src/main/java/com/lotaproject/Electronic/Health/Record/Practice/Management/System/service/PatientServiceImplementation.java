547806429c0b44eb8caa0b4be8dc2126bd10916a
Lotachukwu Onwuka <christiansolomon592@gmail.com>
2023-07-04 22:14:55 +0100

    testing endpoint for updating patient details

--- src/main/java/com/lotaproject/Electronic/Health/Record/Practice/Management/System/service/PatientServiceImplementation.java
+++ src/main/java/com/lotaproject/Electronic/Health/Record/Practice/Management/System/service/PatientServiceImplementation.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -85,7 +86,12 @@ public class PatientServiceImplementation implements PatientService{
     public ApiResponse<?> updatePatientDetails(String id, UpdatePatientDetailRequest request1) {
         var patient = findById(id);
 
-        emailValidation(request1.getEmail());
+//        emailValidation(request1.getEmail());
+        if(!emailIsValid(request1.getEmail())) throw new CannotRegisterPatientException(EMAIL_IS_INVALID.getMessage());
+
+        if(Objects.equals(request1.getEmail(), patient.getEmail())) patient.setEmail(request1.getEmail());
+
+        if(patientRepository.existsByEmail(request1.getEmail())) throw new CannotRegisterPatientException(EMAIL_ALREADY_EXCEPTION.getMessage());
 
         if(request1.getEmail() != null) patient.setEmail(request1.getEmail());
         if(request1.getFirstName() != null) patient.setFirstName(request1.getFirstName());
