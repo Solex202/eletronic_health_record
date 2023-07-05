d638610518913542b581f8af8422df179686976f
Lotachukwu Onwuka <christiansolomon592@gmail.com>
2023-07-05 21:06:51 +0100

    implementing endpoint for finding patient by name

---
+++ src/main/java/com/lotaproject/Electronic/Health/Record/Practice/Management/System/data/dtos/response/PaginatedPatientResponse.java
@@ -0,0 +1,21 @@
+package com.lotaproject.Electronic.Health.Record.Practice.Management.System.data.dtos.response;
+
+import com.lotaproject.Electronic.Health.Record.Practice.Management.System.data.model.Patient;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
+public class PaginatedPatientResponse {
+
+    private List<Patient> patients;
+
+    private int currentPage;
+
+    private int noOfPatients;
+
+    private int noOfTotalPages;
+
+}
