ca2109cc2cace83d2b1c69a910345fae64049ad8
Lotachukwu Onwuka <christiansolomon592@gmail.com>
2023-07-06 16:56:58 +0100

    creating doctor table

---
+++ src/main/java/com/lotaproject/Electronic/Health/Record/Practice/Management/System/data/model/Doctor.java
@@ -0,0 +1,21 @@
+package com.lotaproject.Electronic.Health.Record.Practice.Management.System.data.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document
+public class Doctor {
+
+    @Id
+    private String id;
+
+    private String name;
+}
