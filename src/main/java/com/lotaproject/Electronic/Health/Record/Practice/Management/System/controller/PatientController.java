b7dbcef0b470e0927d26ea7a92eb5a89e0ce96f5
Lotachukwu Onwuka <christiansolomon592@gmail.com>
2023-07-06 17:03:43 +0100

    creating apis for patient service

--- src/main/java/com/lotaproject/Electronic/Health/Record/Practice/Management/System/controller/PatientController.java
+++ src/main/java/com/lotaproject/Electronic/Health/Record/Practice/Management/System/controller/PatientController.java
@@ -1,6 +1,12 @@
 package com.lotaproject.Electronic.Health.Record.Practice.Management.System.controller;
 
 import com.lotaproject.Electronic.Health.Record.Practice.Management.System.data.dtos.request.RegisterPatientRequest;
+import com.lotaproject.Electronic.Health.Record.Practice.Management.System.data.dtos.response.ApiResponse;
+import com.lotaproject.Electronic.Health.Record.Practice.Management.System.data.model.Patient;
+import com.lotaproject.Electronic.Health.Record.Practice.Management.System.exceptions.CannotRegisterPatientException;
+import com.lotaproject.Electronic.Health.Record.Practice.Management.System.service.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,9 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/health-record")
 public class PatientController {
 
+    @Autowired
+    private PatientService patientService;
     @PostMapping("/registration")
     public ResponseEntity<?> register(@RequestBody RegisterPatientRequest registerPatientRequest){
-        return null;
+        try {
+            ApiResponse<?> response = patientService.registerPatient(registerPatientRequest);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (CannotRegisterPatientException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+
+        }
 
     }
 }
