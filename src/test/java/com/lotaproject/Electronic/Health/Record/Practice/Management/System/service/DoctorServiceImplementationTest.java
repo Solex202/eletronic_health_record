e3f69881f43642026a6a03d3591ca103282988d7
Lotachukwu Onwuka <christiansolomon592@gmail.com>
2023-07-07 23:23:52 +0100

    testing doctor service

---
+++ src/test/java/com/lotaproject/Electronic/Health/Record/Practice/Management/System/service/DoctorServiceImplementationTest.java
@@ -0,0 +1,40 @@
+package com.lotaproject.Electronic.Health.Record.Practice.Management.System.service;
+
+import com.lotaproject.Electronic.Health.Record.Practice.Management.System.data.dtos.response.ApiResponse;
+import com.lotaproject.Electronic.Health.Record.Practice.Management.System.data.model.Doctor;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@SpringBootTest
+class DoctorServiceImplementationTest {
+
+    @Autowired
+    private DoctorService doctorService;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void testThatCanCreateDoctor(){
+        Doctor doctor = Doctor.builder().name("doctor jesus").password("password").email("doctor").build();
+        ApiResponse<?> response = doctorService.saveDoctor(doctor);
+
+        assertAll(
+                ()-> assertThat(response.getMessage(), is("Successful"))
+        );
+
+
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
