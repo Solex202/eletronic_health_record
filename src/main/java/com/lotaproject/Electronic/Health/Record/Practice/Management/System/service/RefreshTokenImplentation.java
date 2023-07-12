79581b4656adc18b8e81d62e057e77b9ece4df3d
Lotachukwu Onwuka <christiansolomon592@gmail.com>
2023-07-12 17:06:40 +0100

    auth implementation

---
+++ src/main/java/com/lotaproject/Electronic/Health/Record/Practice/Management/System/service/RefreshTokenImplentation.java
@@ -0,0 +1,7 @@
+package com.lotaproject.Electronic.Health.Record.Practice.Management.System.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RefreshTokenImplentation {
+}
