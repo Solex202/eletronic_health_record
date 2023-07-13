ac8b9ab912f9df5cfc9ee2a5fbb8a788c1b2b320
Lotachukwu Onwuka <christiansolomon592@gmail.com>
2023-07-13 13:30:09 +0100

    auth fixing

---
+++ src/main/java/com/lotaproject/Electronic/Health/Record/Practice/Management/System/exceptions/AuthException.java
@@ -0,0 +1,8 @@
+package com.lotaproject.Electronic.Health.Record.Practice.Management.System.exceptions;
+
+public class AuthException extends RuntimeException{
+
+    public AuthException(String message) {
+        super(message);
+    }
+}
