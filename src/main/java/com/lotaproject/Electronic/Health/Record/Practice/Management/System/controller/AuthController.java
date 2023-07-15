d3e3e1a9d40573d2e839160169da2a51fcab57a8
Lotachukwu Onwuka <christiansolomon592@gmail.com>
2023-07-15 21:27:34 +0100

    creating authentication controller

---
+++ src/main/java/com/lotaproject/Electronic/Health/Record/Practice/Management/System/controller/AuthController.java
@@ -0,0 +1,7 @@
+package com.lotaproject.Electronic.Health.Record.Practice.Management.System.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+}
