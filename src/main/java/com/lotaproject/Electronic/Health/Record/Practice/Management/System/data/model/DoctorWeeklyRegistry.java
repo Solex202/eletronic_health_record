2ce5b4a8e4b87c27aa46e2eae09a69f1b5a01519
Lotachukwu Onwuka <christiansolomon592@gmail.com>
2023-07-18 20:54:06 +0100

    creating doctor schedule

---
+++ src/main/java/com/lotaproject/Electronic/Health/Record/Practice/Management/System/data/model/DoctorWeeklyRegistry.java
@@ -0,0 +1,19 @@
+package com.lotaproject.Electronic.Health.Record.Practice.Management.System.data.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document
+public class DoctorWeeklyRegistry {
+
+    @Id
+    private String id;
+}
