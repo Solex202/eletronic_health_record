2920cd1e80fe8580414f312cc9b81485635aa2e8
Lotachukwu Onwuka <christiansolomon592@gmail.com>
2023-07-19 14:02:28 +0100

    creating doctor schedule

---
+++ src/test/java/com/lotaproject/Electronic/Health/Record/Practice/Management/System/service/DoctorWeeklyRegistryServiceImplTest.java
@@ -0,0 +1,27 @@
+package com.lotaproject.Electronic.Health.Record.Practice.Management.System.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.DayOfWeek;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class DoctorWeeklyRegistryServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void testThatDoctorCanProvideAvailableTimeForWeeklySchedule(){
+
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
