5e37db2de3f4311125bc617fb2e910674c539583
Lotachukwu Onwuka <christiansolomon592@gmail.com>
2023-07-30 13:58:56 +0100

    creating doctor registry

---
+++ src/main/java/com/lotaproject/Electronic/Health/Record/Practice/Management/System/data/model/ScheduleRegistry.java
@@ -0,0 +1,24 @@
+package com.lotaproject.Electronic.Health.Record.Practice.Management.System.data.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ScheduleRegistry {
+
+    @Id
+    private String id;
+    private LocalDateTime from;
+    private  LocalDateTime to;
+    private List<BreakPeriod> breakPeriod;
+}
