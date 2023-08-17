5b5dc346b964e36d520e5f7de63f7f3b41807886
Lotachukwu Onwuka <christiansolomon592@gmail.com>
2023-08-17 12:39:46 +0100

    creating apis for doctor registry

--- src/main/java/com/lotaproject/Electronic/Health/Record/Practice/Management/System/controller/DoctorRegistryController.java
+++ src/main/java/com/lotaproject/Electronic/Health/Record/Practice/Management/System/controller/DoctorRegistryController.java
@@ -1,9 +1,33 @@
 package com.lotaproject.Electronic.Health.Record.Practice.Management.System.controller;
 
+import com.lotaproject.Electronic.Health.Record.Practice.Management.System.data.dtos.response.ApiResponse;
+import com.lotaproject.Electronic.Health.Record.Practice.Management.System.data.model.DoctorRegistry;
+import com.lotaproject.Electronic.Health.Record.Practice.Management.System.service.DoctorRegistryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("")
+@RequestMapping("/doctor-registry")
 public class DoctorRegistryController {
+
+    @Autowired
+    private DoctorRegistryService doctorRegistryService;
+
+    @PostMapping("make-registry")
+    public ResponseEntity<?> createSchedule(@RequestBody DoctorRegistry doctorRegistry){
+
+        try {
+            ApiResponse<?> response = doctorRegistryService.createSchedule(doctorRegistry);
+            return new ResponseEntity<>(response, HttpStatus.CREATED);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
