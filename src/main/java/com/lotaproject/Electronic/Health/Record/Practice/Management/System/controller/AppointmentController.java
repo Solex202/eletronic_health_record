e35c0d43378dea12e2b55742eb7a87f47c1d3c9b
Lotachukwu Onwuka <christiansolomon592@gmail.com>
2023-08-18 22:20:10 +0100

    creating apis to book appointment

---
+++ src/main/java/com/lotaproject/Electronic/Health/Record/Practice/Management/System/controller/AppointmentController.java
@@ -0,0 +1,9 @@
+package com.lotaproject.Electronic.Health.Record.Practice.Management.System.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+public class AppointmentController {
+}
