05b06e3c77325161c6f25dafe0a344f73be6ce63
Lotachukwu Onwuka <christiansolomon592@gmail.com>
2023-08-18 22:27:09 +0100

    creating apis to book appointment

--- src/main/java/com/lotaproject/Electronic/Health/Record/Practice/Management/System/controller/AppointmentController.java
+++ src/main/java/com/lotaproject/Electronic/Health/Record/Practice/Management/System/controller/AppointmentController.java
@@ -1,9 +1,29 @@
 package com.lotaproject.Electronic.Health.Record.Practice.Management.System.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.lotaproject.Electronic.Health.Record.Practice.Management.System.data.dtos.request.BookAppointmentFormDto;
+import com.lotaproject.Electronic.Health.Record.Practice.Management.System.data.dtos.response.ApiResponse;
+import com.lotaproject.Electronic.Health.Record.Practice.Management.System.service.AppointmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("")
 public class AppointmentController {
+
+    @Autowired
+    private AppointmentService appointmentService;
+
+    @PostMapping("book-appointment/{patientId}")
+    public ResponseEntity<?> bookAppointment(@PathVariable String patientId,  @RequestBody BookAppointmentFormDto bookAppointmentFormDto) {
+        try {
+            ApiResponse<?> response = appointmentService.bookAppointment(patientId, bookAppointmentFormDto);
+            return new ResponseEntity<>(response, HttpStatus.CREATED);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
