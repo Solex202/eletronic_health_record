b2c7d256a71efc729e2e52ed3caabcc88be4502e
Lotachukwu Onwuka <christiansolomon592@gmail.com>
2023-08-20 15:16:08 +0100

    implementing reschedule appointment api

---
+++ src/main/java/com/lotaproject/Electronic/Health/Record/Practice/Management/System/exceptions/AppointmentException.java
@@ -0,0 +1,7 @@
+package com.lotaproject.Electronic.Health.Record.Practice.Management.System.exceptions;
+
+public class AppointmentException extends ElectronicHealthException {
+    public AppointmentException(String message) {
+        super(message);
+    }
+}
