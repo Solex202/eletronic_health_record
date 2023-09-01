faec8cee3df5ac927dd192abb2a22ec257900d41
Lotachukwu Onwuka <christiansolomon592@gmail.com>
2023-09-01 16:18:25 +0100

    send confirmation token fix

---
+++ src/test/java/com/lotaproject/Electronic/Health/Record/Practice/Management/System/token/ConfirmationTokenServiceTest.java
@@ -0,0 +1,22 @@
+package com.lotaproject.Electronic.Health.Record.Practice.Management.System.token;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ConfirmationTokenServiceTest {
+
+    @Test
+    void saveConfirmationToken() {
+    }
+
+    @Test
+    void getToken() {
+    }
+
+    @Test
+    void setConfirmedAt() {
+    }
+}
