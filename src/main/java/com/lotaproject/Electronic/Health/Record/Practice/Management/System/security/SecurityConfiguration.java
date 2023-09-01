a0be2ef46247b463632f259e82d6665dc9d84548
Lotachukwu Onwuka <christiansolomon592@gmail.com>
2023-09-01 14:43:54 +0100

    send confirmation token fix

--- src/main/java/com/lotaproject/Electronic/Health/Record/Practice/Management/System/security/SecurityConfiguration.java
+++ src/main/java/com/lotaproject/Electronic/Health/Record/Practice/Management/System/security/SecurityConfiguration.java
@@ -22,8 +22,8 @@ public class SecurityConfiguration {
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
         .csrf(customizer->customizer.disable())
-        .authorizeHttpRequests(c->c.requestMatchers("/health-record/registration", "/health-record/login").permitAll())
-        .authorizeHttpRequests(c->c.requestMatchers("/doctors/register").hasAuthority("DOCTOR"))
+        .authorizeHttpRequests(c->c.requestMatchers("/health-record/registration", "/health-record/login" ,"/doctors/register" ,"/token/confirm").permitAll())
+//        .authorizeHttpRequests(c->c.requestMatchers("/doctors/register").hasAuthority("DOCTOR"))
         .sessionManagement(c->c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authenticationProvider(authenticationProvider)
         .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
