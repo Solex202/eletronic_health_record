1947752271daf904937cc5b283c909d1df058bb1
Lotachukwu Onwuka <christiansolomon592@gmail.com>
2023-09-01 14:43:36 +0100

    send confirmation token fix

---
+++ src/main/java/com/lotaproject/Electronic/Health/Record/Practice/Management/System/controller/ConfirmationTokenController.java
@@ -0,0 +1,22 @@
+package com.lotaproject.Electronic.Health.Record.Practice.Management.System.controller;
+
+import com.lotaproject.Electronic.Health.Record.Practice.Management.System.service.PatientService;
+import com.lotaproject.Electronic.Health.Record.Practice.Management.System.service.PatientServiceImplementation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/token")
+public class ConfirmationTokenController {
+
+    @Autowired
+    private PatientServiceImplementation patientService;
+
+    @GetMapping("/confirm")
+    public String confirm(@RequestParam("token") String token){
+        return patientService.confirmToken(token);
+    }
+}
